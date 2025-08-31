5cca13995f5c254866162430accdbe16bc3920ec
Robanik <168307797+Robanik@users.noreply.github.com>
2025-08-31 18:28:30 +0100

    Update Jumppower.lua

--- Assets/Player/Jumppower.lua
+++ Assets/Player/Jumppower.lua
@@ -1,1 +1,9 @@
-aa
+local player = game.Players.LocalPlayer
+local jumpPower = 55
+
+while true do
+    wait(0.001)  -- обновление максимально часто
+    if player.Character and player.Character:FindFirstChild("Humanoid") then
+        player.Character.Humanoid.JumpPower = jumpPower
+    end
+end
