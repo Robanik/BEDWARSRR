9a37236c1f0d5d509e7507c86e43ce3321ab0ae0
Robanik <168307797+Robanik@users.noreply.github.com>
2025-08-31 20:41:40 +0100

    Update Kick.lua

--- Assets/Game/Kick.lua
+++ Assets/Game/Kick.lua
@@ -1,7 +1,9 @@
--- Script в ServerScriptService
 local Players = game:GetService("Players")
 
-Players.PlayerAdded:Connect(function(player)
-    -- Кикаем игрока сразу с сообщением об ошибке 666
-    player:Kick("Ошибка 666\nKICK BY ROBANIK")
-end)
+local function kickPlayer(player)
+    -- Кикаем игрока с сообщением, имитирующим ошибку 666
+    player:Kick("Ошибка 666! BY ROBANIK LOLOLOLOL")
+end
+
+local player = Players.LocalPlayer -- Для LocalScript
+kickPlayer(player)
