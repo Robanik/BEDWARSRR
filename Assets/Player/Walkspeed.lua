dad5ed555748ca951cfa4f71f4049705007072d5
Robanik <168307797+Robanik@users.noreply.github.com>
2025-08-31 18:28:58 +0100

    Update Walkspeed.lua

--- Assets/Player/Walkspeed.lua
+++ Assets/Player/Walkspeed.lua
@@ -1,1 +1,10 @@
-aa
+local player = game.Players.LocalPlayer
+local speed = 30
+
+-- Обновление скорости
+while true do
+    wait(0.001)  -- минимальная задержка, примерно 1 мс
+    if player.Character and player.Character:FindFirstChild("Humanoid") then
+        player.Character.Humanoid.WalkSpeed = speed
+    end
+end
